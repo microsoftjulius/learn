92f04b393896f7b07c463ec44bb14172d5112a54
microsoftjulius <julisema4@gmail.com>
2023-01-09 15:02:20 +0300

    Can read posts from the db, added posts via the django admin

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from pages.models import Pages
+
 # Register your models here.
+admin.site.register(Pages)
--- pages/views.py
+++ pages/views.py
@@ -1,11 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+
+from pages.models import Pages
 
 
 # Create your views here.
-class HomePageView(TemplateView):
+class HomePageView(ListView):
+    model = Pages
     template_name = 'pages/home.html'
+    context_object_name = 'all_posts_list'
 
 
 class AboutPageView(TemplateView):
