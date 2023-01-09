a9b97ec1c9e2876e7b7f9f09df15534f6d0c431f
microsoftjulius <julisema4@gmail.com>
2023-01-09 13:42:43 +0300

    Added the virtual env Also, migrated, printed hello world

---
+++ pages/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+
+from pages.views import home_page_view
+
+urlpatterns = [
+    path('',home_page_view, name="home"),
+]
--- test_project/settings.py
+++ test_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pages.apps.PagesConfig'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'test_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'learn_django',
+        'USER': 'admin',
+        'PASSWORD': '123Jane14.',
+        'HOST': '127.0.0.1',
+        'PORT': '5435',
     }
 }
 
